c87d8a7ef2f29af8f8858ccb94e26b889a18a46d
HarryLemoelleu <harry.lemoelleu@gmail.com>
2022-08-08 10:35:58 +0200

    php

---
+++ footer.php
@@ -0,0 +1,42 @@
+<footer class="footer-grid">
+    <article class="specialites">
+      <p class="F1">NOS SPECIALITÉS</p>
+      <p class="F2">LOCALES</p>
+      <p class="F2">NATIONALES</p>
+      <p class="F2">LE MONDE</p>
+    </article>
+    <article class="aide">
+      <p class="F1">AIDE</p>
+      <p class="F2">FAQ</p>
+      <p class="F2">MENTIONS LÉGALES</p>
+      <a class="F2" href="./contact.html">
+        <p>CONTACT</p>
+      </a>
+    </article>
+    <article class="panda-tea">
+      <p class="F1">PANDA TEA</p>
+      <a class="F2" href="./nos_packs.html">
+        <p>NOS PACKS</p>
+      </a>
+      <p class="F2">PRESSE</p>
+      <a class="F2" href="./a_propos.html">
+        <p>A PROPOS</p>
+      </a>
+      <a class="F2" href="./forums.html">
+        <p>FORUMS</p>
+      </a>
+    </article>
+    <article class="reseaux">
+      <p class="F1">SUIVEZ-NOUS SUR LES RÉSEAUX SOCIAUX</p>
+      <img src="./img/icon-instagram.svg" alt="instagram">
+      <img src="./img/icon-facebook.svg" alt="facebook">
+      <img src="./img/icon-pinterest.svg" alt="pinterest">
+    </article>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Accueil</title>
+  <link rel="icon" href="./img/compte-panda-tea.svg">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="accueil.css">
+</head>
+
+<body>
+  <!------------------------------------------------------BOOTSTRAP--------------------------------------------------------->
+  <header>
+    <nav class="navbar fixed-top navbar-expand-lg" id="nav-bar">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="./index.html"><img
+            src="./img/compte-panda-tea.svg" alt="logo site"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="./nos_produits.html" id="menu-produits">Nos produits</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./nos_packs.html" id="menu-packs">Nos packs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./a_propos.html" id="menu-propos">A propos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./forums.html" id="menu-forums">Forums</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./contact.html" id="menu-contact">Contact</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav text-center">
+            <li id="nav-ico">
+              <a class="nav-link sup-padding" href="./espace_client.html"><img class="img-compte"
+                  src="./img/compte.png" alt="espace client"></a>
+            </li>
+            <li id="nav-ico">
+              <a class="nav-link sup-padding" href="#"><img class="img-panier"
+                  src="./img/panier.png" alt="panier"></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
